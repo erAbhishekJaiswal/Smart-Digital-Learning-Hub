5e9712661131286552f2c0156b4d658cd94d1628
erAbhishekJaiswal <erabhishek72@gmail.com>
2025-11-12 23:18:49 +0530

    update the error

--- src/pages/Admin/techStack/AdminTechstacklist.jsx
+++ src/pages/Admin/techStack/AdminTechstacklist.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import "./AdminTechstacklist.css";
+import "./AdminTechStackList.css";
 import "../../../CSSFiles/PublicPages/TechStackPage.css";
 import { getToken, getUserRole } from "../../../utils/localstorage";
 
