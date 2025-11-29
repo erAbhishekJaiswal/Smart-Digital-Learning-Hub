73abd0a742dfe65d472b533e2957f42f7b7247e0
erAbhishekJaiswal <erabhishek72@gmail.com>
2025-11-29 14:23:48 +0530

    update and add the visitor countor

--- src/pages/PublicPages/Home.jsx
+++ src/pages/PublicPages/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../../CSSFiles/PublicPages/Home.css';
 import Header from '../../Components/PublicComp/Header';
 import Footer from '../../Components/PublicComp/Footer';
 import ELibraryBooks from './ELibraryBooks';
+import axios from 'axios';
+
+const BasseUrl = import.meta.env.VITE_BASE_URL
 function App() {
+    useEffect(() => {
+    const today = new Date().toISOString().split("T")[0];
+    console.log(today);
+    
+    const visited = sessionStorage.getItem("visitedDate");
+    console.log(visited);
+    
+    if (visited === today) return; // already counted today
+    const response = axios.post(`${BasseUrl}/visitors/count`);
+    console.log(response.data);
+    
+    sessionStorage.setItem("visitedDate", today);
+  }, []);
   return (
     <div className="home-page">
     {/* <div className="home-page__container">
